6f70bbac8ae4094ebee14e0d56dd2d79474899f8
taylersala <taylersala@gmail.com>
2024-11-03 14:58:11 -0800

    graph and header completed

--- src/components/pages/DataVisualizations/Graphs/TableComponents/TableRow.jsx
+++ src/components/pages/DataVisualizations/Graphs/TableComponents/TableRow.jsx
@@ -37,6 +37,7 @@ function TableRow(props) {
             );
           }
         } else {
+        }
           return <></>;
       })}
     </div>
--- src/components/pages/Landing/RenderLandingPage.jsx
+++ src/components/pages/Landing/RenderLandingPage.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 // ADD IMPORTS BACK FOR GRAPHS SECTION
-// import GrantRatesByOfficeImg from '../../../styles/Images/bar-graph-no-text.png';
-// import GrantRatesByNationalityImg from '../../../styles/Images/pie-chart-no-text.png';
-// import GrantRatesOverTimeImg from '../../../styles/Images/line-graph-no-text.png';
+import GrantRatesByOfficeImg from '../../../styles/Images/bar-graph-no-text.png';
+import GrantRatesByNationalityImg from '../../../styles/Images/pie-chart-no-text.png';
+import GrantRatesOverTimeImg from '../../../styles/Images/line-graph-no-text.png';
 import HrfPhoto from '../../../styles/Images/paper-stack.jpg';
 import '../../../styles/RenderLandingPage.less';
 import { Button } from 'antd';
 import { useHistory } from 'react-router-dom';
+
 // for the purposes of testing PageNav
 // import PageNav from '../../common/PageNav';
 
@@ -32,7 +33,28 @@ function RenderLandingPage(props) {
       </div>
 
       {/* Graphs Section: Add code here for the graphs section for your first ticket */}
-      {/* <div className="graphs-section"> */}
+      <div className="graphs-section">
+        <div className="graph-items">
+          <div className="graph-image-container">
+          <img src={GrantRatesByOfficeImg} alt="Bar graph with no text" className="graph-image-office"/>
+          </div>
+          <p>Search Grant Rates By Office</p>
+        </div>
+        <div className="graph-items">
+          <div className="graph-image-container">
+          <img src={GrantRatesByNationalityImg} alt="Pie chart with no text" className="graph-image-nationality"/>
+          </div>
+          <p>Search Grant Rates By Nationality</p>
+        </div>
+        <div className="graph-items">
+          <div className="graph-image-container">
+          <img src={GrantRatesOverTimeImg} alt="Line graph with no text" className="graph-image-time"/>
+          </div>
+          <p>Search Grant Rates By Over Time</p>
+        </div>
+      </div>
+      
+      <div className="button-container">
       <div className="view-more-data-btn-container">
         <Button
           type="default"
@@ -41,6 +63,17 @@ function RenderLandingPage(props) {
         >
           View the Data
         </Button>
+      </div>
+      <div className="download-data-btn-contasiner">
+        <a href="../../../data/COW2021001887-I589Data.csv" download>
+        <Button
+          type="default"
+          style={{ backgroundColor: '#404C4A', color: '#FFFFFF' }}
+        >
+          Download the Data
+        </Button>
+        </a>
+      </div>
       </div>
 
       <div className="middle-section">
@@ -61,7 +94,28 @@ function RenderLandingPage(props) {
       </div>
       <div>
         {/* Bottom Section: Add code here for the graphs section for your first ticket */}
-        {/* <div className="bottom-section">*/}
+        <div className="bottom-section">
+          <div className="first-data-point-container">
+            <h2>36%</h2>
+            <h3>
+            By the end of the Trump administration, the average asylum office grant rate had fallen 36 percent from an average of 44 percent in fiscal year 2016 to 28 percent in fiscal year 2020.
+            </h3>
+          </div>
+          <div className="second-data-point-container">
+            <h2>5%</h2>
+            <h3>
+            The New York asylum office grant rate dropped to 5 percent in fiscal year 2020.
+            </h3>
+          </div>
+          <div className="third-data-point-container">
+            <h2>6x Lower</h2>
+            <h3>
+            Between fiscal year 2017 and 2020, the New York asylum office’s average grant rate was six times lower than the San Francisco asylum office.
+            </h3>
+          </div>
+
+          
+        </div>
         <p onClick={() => scrollToTop()} className="back-to-top">
           Back To Top ^
         </p>
